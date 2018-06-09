495978f9234d82b3795fe4c2c68cdb278994f392
dani barria <barriareyesjosedaniel@gmail.com>
2018-06-09 01:55:10 -0300

    eventloop javascript

---
+++ 06-asincrono/01-eventloop.js
@@ -0,0 +1,6 @@
+//en si js es de un solo hilo (una sola cosa a la vez)
+// callstack
+
+//delegacion (permite usar )
+//      cola de tarea (task queue)
+//      los callback nos permiten cargar en la cola de tareas
---
+++ 06-asincrono/02-callback.js
@@ -0,0 +1,12 @@
+// const time = 1000
+// setTimeout(function callback(){
+//     console.log(`ya paso ${time} milisegundo`)
+// }, time) // 1 seg
+// console.log(`Hola`)
+
+const zero = 0
+
+setTimeout(function llamada(){
+    console.log(`prueba con ${zero}`)
+},zero)
+console.log(`Hola`)
